68b6fb15e7655842cff65d42f7e6641e498e18bd
rts-Liva <U2451012@rts-365.dk>
2025-06-11 12:16:02 +0200

    featured page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,28 @@
+import Header from "../components/header";
+import albums from "../../json/albums.json";
+import Footer from "../components/footer";
+
 function HomePage() {
     return (
         <>
-            <h1>home</h1>
+            <Header>featured</Header>
+            <main>
+                <h2 className="heading heading--gradient">featured</h2>
+                <div className="featured-album">
+                    {albums?.featuredAlbums?.length > 0 ? (
+                        albums?.featuredAlbums?.map(album => (
+                            <article className="featured-album-card" key={album.id}>
+                                <img src={album.imagePath} alt={`${album.title} cover`} className="featured-album-card__cover" />
+                                <section className="featured-album-card__text">
+                                    <h3 className="featured-album-card__title">{album.title}</h3>
+                                    <p className="text text--light">{album.description}</p>
+                                </section>
+                            </article>
+                        ))
+                    ) : <p>No featured albums found...</p>}
+                </div>
+            </main>
+            <Footer current='albums' />
         </>
     );
 }
