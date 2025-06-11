9f38f123de10d56bb9e3a41e8d805ba88523688f
rts-Liva <U2451012@rts-365.dk>
2025-06-11 12:16:12 +0200

    header

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import { IoIosArrowBack, IoIosSearch } from "react-icons/io";
+import { useNavigate } from "react-router";
+
+function Header({ children: title, colour = 'dark' }) {
+    const navigate = useNavigate();
+
+    return (
+        <header className="header">
+            <IoIosArrowBack onClick={() => navigate(-1)} className={colour === 'dark' ? 'header__icon' : 'header__icon header__icon--light'} />
+            <h1 className={colour === 'dark' ? 'header__title' : 'header__title header__title--light'}>{title}</h1>
+            <IoIosSearch className={colour === 'dark' ? 'header__icon' : 'header__icon header__icon--light'} />
+        </header>
+    );
+}
+
+export default Header;
