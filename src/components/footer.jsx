4315409132b6254018047c3dcc98760d6faf5ad4
rts-Liva <U2451012@rts-365.dk>
2025-06-12 08:28:55 +0200

    fix styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ function Footer({ current }) {
     return (
         <footer className="footer">
             <nav>
-                <svg width='0' height='0'>
+                <svg width='0' height='0' className="footer__gradient">
                     <linearGradient id="gradient" x1='0%' y1='0%' x2='100%' y2='0%'>
                         <stop stopColor="#EE0979" offset='0%' />
                         <stop stopColor="#F2BC06" offset='100%' />
